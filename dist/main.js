d7506bd9dc050a083cd8bf223bcaedb836d3804b
EkoRamadn <ekoramdani7777@gmail.com>
2025-01-22 13:18:16 +0700

    basic config

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";document.addEventListener("click",void alert("hello")),document.body.appendChild(function(){const e=document.createElement("div");return e.innerHTML=_.join(["Hello","webpack"]," "),e}())})();
