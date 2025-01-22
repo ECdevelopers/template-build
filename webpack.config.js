1b18c531c36a95c5c37262ba87215a7e195d2c44
EkoRamadn <ekoramdani7777@gmail.com>
2025-01-22 13:38:14 +0700

    babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,19 @@ module.exports = {
         use: [
           'style-loader','css-loader'
         ]
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            targets: "defaults",
+            presets: [
+              ['@babel/preset-env']
+            ]
+          }
+        }
       }
     ]
   }
