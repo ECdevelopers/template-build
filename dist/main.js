9e2be74051dc29ab3da9a59f4763bbb584bd702f
EkoRamadn <ekoramdani7777@gmail.com>
2025-01-22 13:04:20 +0700

    inisialisasi webpack

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.addEventListener("click",void alert("hello")),document.body.appendChild(function(){const e=document.createElement("div");return e.innerHTML=_.join(["Hello","webpack"]," "),e}())})();
---
+++ src/buttonHandler.js
@@ -0,0 +1,3 @@
+export const buttonHandle = ()=>{
+  alert('hello')
+}
